561a26db9a26b8dac21b65ad57eb5fca1a094990
BalleNz <66498314+BalleNz@users.noreply.github.com>
2021-03-10 06:28:37 +0300

    Update meh.py

--- meh.py
+++ meh.py
@@ -11,7 +11,7 @@ from itertools import *
 for j in permutations([*map(int, input().split())]):
     for x in product('*-+%/', repeat = len(j)-1):
         nm = [j[i] for i in range(len(j))]
-        temp = nm[0]; 
-        for q in range(len(x)//2+2 if not len(x)%2 else len(x)//2+1):
+        temp = nm[0];
+        for q in range(len(j)//2 if not len(j)%2 else len(j)//2+1):
             temp = ops[x[q]](temp, nm[q+1])
         if temp == end: print(j, x)
