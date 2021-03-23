dac39e3f384eb812727ae28701b7ddac532b99cf
BalleNz <66498314+BalleNz@users.noreply.github.com>
2021-03-23 19:35:23 +0300

    Update meh.py

--- meh.py
+++ meh.py
@@ -6,12 +6,14 @@ ops = {
     '/' : operator.floordiv,
     '%' : operator.mod,
 }
-end = int(input())
-from itertools import *
-for j in permutations([*map(int, input().split())]):
-    for x in product('*-+%/', repeat = len(j)-1):
-        nm = [j[i] for i in range(len(j))]
-        temp = nm[0];
-        for q in range(len(j)//2 if not len(j)%2 else len(j)//2+1):
-            temp = ops[x[q]](temp, nm[q+1])
-        if temp == end: print(j, x)
+while True:
+    end = int(input())
+    from itertools import *
+    for j in permutations([*map(int, input().split())]):
+        for ln in range(2, len(j)):
+            for x in product('*-+%/', repeat = ln-1):
+                nm = [j[i] for i in range(ln)]
+                temp = nm[0];
+                for q in range(ln//2 if not ln%2 else ln//2+1):
+                    temp = ops[x[q]](temp, nm[q+1])
+                if temp == end: print(j[:ln], x)
