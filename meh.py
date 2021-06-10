76810d3d169ac3bec98dbf795cebc36fa43021cb
BalleNz <66498314+BalleNz@users.noreply.github.com>
2021-06-10 13:30:09 +0300

    Update meh.py

--- meh.py
+++ meh.py
@@ -1,19 +1,22 @@
+from random import randint
 import operator
+from itertools import *
 ops = {
     '+' : operator.add,
     '-' : operator.sub,
     '*' : operator.mul,
     '/' : operator.floordiv,
-    '%' : operator.mod,
 }
-while True:
-    end = int(input())
-    from itertools import *
-    for j in permutations([*map(int, input().split())]):
-        for ln in range(2, len(j)):
-            for x in product('*-+%/', repeat = ln-1):
-                nm = [j[i] for i in range(ln)]
-                temp = nm[0];
-                for q in range(ln//2 if not ln%2 else ln//2+1):
-                    temp = ops[x[q]](temp, nm[q+1])
-                if temp == end: print(j[:ln], x)
+def f(end):
+    c_temp, c, k = 0, 0, (end+100)//2**(len(str(end))+2)
+    while c < 100:
+        for j in permutations([randint(1, 2*end) for _ in range(2, k)]):
+            if c%5: c += 1; break
+            if c_temp > 50: c_temp = 0;  break
+            for x in product('*-+/', repeat = len(j)-1):
+                nm = [j[i] for i in range(len(j))]; temp = nm[0]
+                for q in range(len(j)-1): temp = ops[x[q]](temp, nm[q+1])
+                if temp != end: c_temp += 1
+                elif temp == end:
+                    c += 1; print(*list(map(lambda x: ''.join(x), sum([[str(j[i]), x[i]] if i != len(x) else [str(j[i])] for i in range(len(j))], []))), sep = '')
+f(int(input()))
